70d9a3e9fc23f50176d893db8c8d507dab28bdd2
PeterT <tranpp87@gmail.com>
2022-03-29 23:21:31 -0400

    declare variables and setup randomSquare function

---
+++ index.js
@@ -0,0 +1,16 @@
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const score = document.getElementById('score')
+const timeLeft = document.getElementById('time-left')
+
+let result = 0
+
+function randomSquare() {
+  squares.forEach(square => {
+    square.classList.remove('mole')
+  })
+  let randomSquare = squares[Math.floor(Math.random() * 9)]
+  randomSquare.classList.add('mole')
+}
+
+randomSquare()
