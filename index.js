1876b91901c103a74880d331ffd4b2f7f724e4fb
PeterT <tranpp87@gmail.com>
2022-03-30 09:59:06 -0400

    add function to get score after clicking on mole

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-const squares = document.querySelectorAll('.square')
-const mole = document.querySelector('.mole')
-const score = document.getElementById('score')
-const timeLeft = document.getElementById('time-left')
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const score = document.getElementById("score");
+const timeLeft = document.getElementById("time-left");
 
-let result = 0
+let result = 0;
+let hitPosition;
 
 function randomSquare() {
-  squares.forEach(square => {
-    square.classList.remove('mole')
-  })
-  let randomSquare = squares[Math.floor(Math.random() * 9)]
-  randomSquare.classList.add('mole')
+  squares.forEach((square) => {
+    square.classList.remove("mole");
+  });
+  let randomSquare = squares[Math.floor(Math.random() * 9)];
+  randomSquare.classList.add("mole");
+
+  hitPosition = randomSquare.id;
 }
 
+squares.forEach((square) => {
+  square.addEventListener("mousedown", () => {
+    if (square.id == hitPosition) {
+      result++;
+      score.textContent = result;
+      hitPosition = null;
+    }
+  });
+});
+
 function moveMole() {
-  let timerID = null
-  timerID = setInterval(randomSquare, 500)
+  let timerID = null;
+  timerID = setInterval(randomSquare, 1000);
 }
+
+moveMole();
