495eb4e990a3b384a94732e036645061c50fdcdb
PeterT <tranpp87@gmail.com>
2022-03-30 10:08:50 -0400

    add countdown feature and add alert for final score

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const timeLeft = document.getElementById("time-left");
 
 let result = 0;
 let hitPosition;
+let timerID = null;
+let currentTime = 60
 
+//randomly selecting a square and removing/adding class of 'mole'
 function randomSquare() {
   squares.forEach((square) => {
     square.classList.remove("mole");
@@ -13,9 +16,11 @@ function randomSquare() {
   let randomSquare = squares[Math.floor(Math.random() * 9)];
   randomSquare.classList.add("mole");
 
+  //making hitPosition the same as the 'mole' for scoring
   hitPosition = randomSquare.id;
 }
 
+//to accumulate score when clicking on mole
 squares.forEach((square) => {
   square.addEventListener("mousedown", () => {
     if (square.id == hitPosition) {
@@ -26,9 +31,22 @@ squares.forEach((square) => {
   });
 });
 
+//moving mole randomly at 1000ms
 function moveMole() {
-  let timerID = null;
   timerID = setInterval(randomSquare, 1000);
 }
 
 moveMole();
+
+function countDown() {
+  currentTime--
+  timeLeft.textContent = currentTime
+
+  if (currentTime == 0){
+    clearInterval(countDownTimerId)
+    clearInterval(timerID)
+    alert(`GAME OVER! FINAL SCORE: ${result}`)
+  }
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
